0197f7b2da97737e5fec765c11967e9c220f999b
Asbjørn Munk <asbjorn@hey.com>
2023-11-04 15:14:53 +0100

    Add a simple test

---
+++ tests/test_infinite_random_batch_sampler.py
@@ -0,0 +1,15 @@
+import numpy as np
+from yucca.training.data_loading.samplers import InfiniteRandomBatchSampler
+
+
+def test_inifite_random_sampler():
+    dataset = np.arange(100)
+    batch_size = 10
+    sampler = InfiniteRandomBatchSampler(dataset, batch_size)
+
+    # repeat test 5 times
+    for i in range(5):
+        sample = list(sampler)[0]
+
+        assert len(sample) == batch_size
+        assert len(set(sample)) == batch_size  # no duplicates
