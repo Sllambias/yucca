3201da37dca8dea8d4377b1ff1c9a16b28d893d1
zcr545 <snl@di.ku.dk>
2023-11-04 18:49:54 +0100

    Mostly fixing tabs -> spaces

--- yucca/training/data_loading/YuccaDataModule.py
+++ yucca/training/data_loading/YuccaDataModule.py
@@ -3,69 +3,81 @@ import random
 import numpy as np
 import torchvision
 from torch.utils.data import DataLoader
-from batchgenerators.utilities.file_and_folder_operations import subfiles
-from yucca.training.data_loading.YuccaDataset import YuccaTrainDataset, YuccaTestDataset
+from batchgenerators.utilities.file_and_folder_operations import subfiles, join
+from yucca.training.data_loading.YuccaDataset import YuccaTrainIterableDataset, YuccaTestDataset
 from yucca.training.data_loading.samplers import InfiniteRandomBatchSampler
 from yucca.preprocessing.YuccaPreprocessor import YuccaPreprocessor
+from yucca.training.trainers.YuccaConfigurator import YuccaConfigurator
+
 
 class YuccaDataModule(pl.LightningDataModule):
-	def __init__(self, 
-			  train_data_dir: str = None, 
-			  pred_data_dir: str = None,
-			  batch_size: int = 2,
-			  composed_train_transforms: torchvision.transforms.Compose = None,
-			  composed_val_transforms: torchvision.transforms.Compose = None,
-			  patch_size: list | tuple = None,
-			  generator_patch_size: list | tuple = None
-			  ):
-		super().__init__()
-		self.batch_size = batch_size
-		self.train_data_dir = train_data_dir
-		self.pred_data_dir = pred_data_dir
-		self.composed_train_transforms = composed_train_transforms
-		self.composed_val_transforms = composed_val_transforms
-		self.patch_size = patch_size
-		self.generator_patch_size = generator_patch_size
-		self.sampler = InfiniteRandomBatchSampler
-		
-	def prepare_data(self):
-		pass
+    def __init__(
+            self,
+            configurator: YuccaConfigurator,
+            pred_data_dir: str = None,
+            composed_train_transforms: torchvision.transforms.Compose = None,
+            composed_val_transforms: torchvision.transforms.Compose = None,
+            ):
+        super().__init__()
+        # First set our configurator object
+        self.cfg = configurator
 
-	def setup(self, stage: str = 'fit'):
-		expected_stages = ['fit', 'test', 'predict']
-		assert stage in expected_stages, "unexpected stage. "\
-			f"Expected: {expected_stages} and found: {stage}"
-		
-		# Assign train/val datasets for use in dataloaders
-		if stage == 'fit':
-			self.train_dataset = YuccaTrainDataset(
-				self.train_data_dir, 
-				keep_in_ram=True,
-				composed_transforms=self.composed_train_transforms,
-				patch_size=self.generator_patch_size)
-			
-			self.val_dataset = YuccaTrainDataset(
-				self.train_data_dir,
-				keep_in_ram=True,
-				composed_transforms=self.composed_val_transforms,
-				patch_size=self.patch_size)
-			
-		if stage == 'predict':
-			self.pred_dataset = YuccaTestDataset(
-				self.pred_data_dir,
-				patch_size=self.patch_size)
-		
-	def train_dataloader(self):
-		train_sampler = self.sampler(self.train_dataset, batch_size=self.batch_size)
-		return DataLoader(self.train_dataset, num_workers=0, batch_sampler=train_sampler)
+        # Now extract parameters from the cfg
+        self.batch_size = self.cfg.batch_size
+        self.initial_patch_size = self.cfg.initial_patch_size
+        self.patch_size = self.cfg.patch_size
+        self.train_data_dir = self.cfg.train_data_dir
+        self.train_split = self.cfg.train_split
+        self.val_split = self.cfg.val_split
 
-	def val_dataloader(self):
-		val_sampler = self.sampler(self.val_dataset, batch_size=self.batch_size)
-		return DataLoader(self.val_dataset, num_workers=0, batch_sampler=val_sampler)
+        # Set by initialize()
+        self.composed_train_transforms = composed_train_transforms
+        self.composed_val_transforms = composed_val_transforms
 
-	def test_dataloader(self):
-		return None
+        # Set in the predict loop
+        self.pred_data_dir = pred_data_dir
 
-	def predict_dataloader(self):
-		return DataLoader(self.pred_dataset, batch_size=1)
+        # Set default values
+        self.sampler = InfiniteRandomBatchSampler
 
+    def prepare_data(self):
+        self.train_samples = [join(self.train_data_dir, i) for i in self.train_split]
+        self.val_samples = [join(self.train_data_dir, i) for i in self.val_split]
+
+    def setup(self, stage: str = 'fit'):
+        expected_stages = ['fit', 'test', 'predict']
+        assert stage in expected_stages, "unexpected stage. "\
+            f"Expected: {expected_stages} and found: {stage}"
+
+        # Assign train/val datasets for use in dataloaders
+        if stage == 'fit':
+            self.train_dataset = YuccaTrainIterableDataset(
+                self.train_samples,
+                keep_in_ram=True,
+                composed_transforms=self.composed_train_transforms,
+                patch_size=self.initial_patch_size)
+
+            self.val_dataset = YuccaTrainIterableDataset(
+                self.val_samples,
+                keep_in_ram=True,
+                composed_transforms=self.composed_val_transforms,
+                patch_size=self.patch_size)
+
+        if stage == 'predict':
+            self.pred_dataset = YuccaTestDataset(
+                self.pred_data_dir,
+                patch_size=self.patch_size)
+
+    def train_dataloader(self):
+        train_sampler = self.sampler(self.train_dataset, batch_size=self.batch_size)
+        return DataLoader(self.train_dataset, num_workers=8, batch_sampler=train_sampler)
+
+    def val_dataloader(self):
+        val_sampler = self.sampler(self.val_dataset, batch_size=self.batch_size)
+        return DataLoader(self.val_dataset, num_workers=4, batch_sampler=val_sampler)
+
+    def test_dataloader(self):
+        return None
+
+    def predict_dataloader(self):
+        return DataLoader(self.pred_dataset, batch_size=1)
--- yucca/training/trainers/YuccaLightningManager.py
+++ yucca/training/trainers/YuccaLightningManager.py
@@ -1,16 +1,12 @@
 #%%
-import yucca
 import lightning as L
-import numpy as np
-import os
-from batchgenerators.utilities.file_and_folder_operations import join, subfiles
 from yucca.training.augmentation.YuccaAugmentationComposer import YuccaAugmentationComposer
 from yucca.training.data_loading.YuccaDataModule import YuccaDataModule
 from yucca.training.trainers.YuccaConfigurator import YuccaConfigurator
 from yucca.training.trainers.YuccaLightningModule import YuccaLightningModule
-from yucca.preprocessing.YuccaPreprocessor import YuccaPreprocessor
-from yucca.paths import yucca_preprocessed, yucca_results
-from yuccalib.utils.files_and_folders import recursive_find_python_class, BatchWriteSegFromLogits
+from yucca.paths import yucca_results
+from yuccalib.utils.files_and_folders import recursive_find_python_class, WriteSegFromLogits
+
 
 class YuccaLightningManager:
     """
@@ -48,7 +44,7 @@ class YuccaLightningManager:
     | 
     """
     def __init__(
-            self, 
+            self,
             continue_training: bool = None,
             deep_supervision: bool = False,
             folds: int = 0,
@@ -59,7 +55,7 @@ class YuccaLightningManager:
             ckpt_path: str = None,
             **kwargs
             ):
-        
+
         self.continue_training = continue_training
         self.deep_supervision = deep_supervision
         self.folds = folds
@@ -76,14 +72,16 @@ class YuccaLightningManager:
         self.kwargs = kwargs
         self.trainer = L.Trainer
 
-    def initialize(self, 
-            train=False, 
-            pred_data_dir: str = None, 
+    def initialize(
+            self,
+            train=False,
+            pred_data_dir: str = None,
             segmentation_output_dir: str = './'):
         # Here we configure the outpath we will use to store model files and metadata
         # along with the path to plans file which will also be loaded.
+        print("initializing, train is:", train)
         configurator = YuccaConfigurator(
-            folds = self.folds,
+            folds=self.folds,
             model_dimensions=self.model_dimensions,
             model_name=self.model_name,
             planner=self.planner,
@@ -94,26 +92,20 @@ class YuccaLightningManager:
             is_2D=False)
 
         self.model_module = YuccaLightningModule(
-            num_classes = configurator.num_classes,
-            num_modalities = configurator.num_modalities,
-            patch_size = configurator.patch_size,
-            plans_path = configurator.plans_path,
+            num_classes=configurator.num_classes,
+            num_modalities=configurator.num_modalities,
+            patch_size=configurator.patch_size,
+            plans_path=configurator.plans_path,
             test_time_augmentation=bool(augmenter.mirror_p_per_sample)
             )
 
-        #if self.ckpt_path:
-        #    self.model_module = YuccaLightningModule.load_from_checkpoint(self.ckpt_path)
-
         self.data_module = YuccaDataModule(
-            train_data_dir=configurator.train_data_dir,
+            configurator=configurator,
             pred_data_dir=pred_data_dir,
-            batch_size=configurator.batch_size,
             composed_train_transforms=augmenter.train_transforms,
-            composed_val_transforms=augmenter.val_transforms,
-            patch_size = configurator.patch_size,
-            generator_patch_size=configurator.initial_patch_size)
-        
-        pred_writer = BatchWriteSegFromLogits(output_dir=segmentation_output_dir, write_interval="batch")
+            composed_val_transforms=augmenter.val_transforms)
+
+        pred_writer = WriteSegFromLogits(output_dir=segmentation_output_dir, write_interval="batch")
         self.trainer = L.Trainer(
             default_root_dir=configurator.outpath,
             callbacks=[pred_writer],
@@ -121,30 +113,29 @@ class YuccaLightningManager:
 
     def run_training(self):
         self.initialize(train=True)
-        self.trainer.fit(model = self.model_module, 
-                         datamodule=self.data_module, 
+        self.trainer.fit(model=self.model_module,
+                         datamodule=self.data_module,
                          ckpt_path=self.ckpt_path)
 
     def predict_folder(self, input_folder, output_folder: str = yucca_results, not_strict=True, save_softmax=False,
                        overwrite=False, do_tta=False):
-        self.initialize(train=False, 
-                        pred_data_dir = input_folder, 
-                        segmentation_output_dir = output_folder)
-        self.trainer.predict(model = self.model_module, 
-                             dataloaders=self.data_module, 
+        self.initialize(train=False,
+                        pred_data_dir=input_folder,
+                        segmentation_output_dir=output_folder)
+        self.trainer.predict(model=self.model_module,
+                             dataloaders=self.data_module,
                              ckpt_path=self.ckpt_path)
 
 
-
-
-#if __name__ == '__main__':
+if __name__ == '__main__':
 #path ='/Users/zcr545/Desktop/Projects/repos/yucca_data/models/Task001_OASIS/UNet__YuccaPlanner/3D/YuccaConfigurator/0/lightning_logs/version_0/checkpoints/epoch=0-step=1.ckpt'
 #path = r'C:\Users\Sebastian\Desktop\work\YuccaData\yucca_models\Task001_OASIS\TinyUNet__YuccaPlanner\3D\YuccaConfigurator\0\lightning_logs\version_46\checkpoints\epoch=9-step=10.ckpt'
-path = None
-Manager = YuccaLightningManager(task = 'Task001_OASIS', min_steps=1000*250, ckpt_path=path)
-Manager.initialize()
-Manager.run_training()
+    path = None
+    Manager = YuccaLightningManager(task = 'Task001_OASIS', max_epochs=1000, min_steps=250, limit_val_batches=50,
+                                    max_epochs=10, ckpt_path=path)
 
-from yucca.paths import yucca_raw_data
+    Manager.run_training()
+
 #Manager.predict_folder(join(yucca_raw_data, 'Task001_OASIS/imagesTs'))
-#%%
+
+# %%
