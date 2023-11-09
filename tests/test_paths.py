5e7adaa867b5abc071d14359b65dc90f4b9d49c0
Asbjørn Munk <asbjorn@hey.com>
2023-11-09 10:23:25 +0100

    Fix minor bug in test

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -1,4 +1,10 @@
 def test_loads_env_var():
+<<<<<<< Updated upstream
     from yucca.paths import yucca_preprocessed
 
     assert yucca_preprocessed is not None
+=======
+    from yucca.paths import yucca_raw_data
+
+    assert yucca_raw_data is not None
+>>>>>>> Stashed changes
