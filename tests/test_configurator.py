604494d4f85d56d62370fee0d9fe825cdbec38ea
LlambiasMBP <llambias@live.com>
2023-11-16 12:58:31 +0100

    debug

--- tests/test_configurator.py
+++ tests/test_configurator.py
@@ -3,6 +3,14 @@ def test_manager_works():
     import os
     from yucca.training.trainers.YuccaConfigurator import YuccaConfigurator
 
+    print(os.path.isdir("/home"))
+    print(os.path.isdir("/home/runner"))
+    print(os.path.isdir("/home/runner/work"))
+    print(os.path.isdir("/home/runner/work/yucca"))
+    print(os.path.isdir("/home/runner/work/yucca/tests"))
+    print(os.path.isdir("/home/runner/work/yucca/tests/data"))
+    print(os.path.isdir("/home/runner/work/yucca/tests/data/Task000_Test"))
+    print(os.path.isdir("/home/runner/work/yucca/tests/data/Task000_Test/YuccaPlanner"))
     print(os.getcwd())
 
     configurator = YuccaConfigurator(task="Task000_Test")
