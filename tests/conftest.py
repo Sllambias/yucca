80acd5154a3fdc7f5e8254b924bbd6292920c6b0
LlambiasMBP <llambias@live.com>
2023-11-16 13:36:44 +0100

    .

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,17 @@
 import pytest
 from dotenv import load_dotenv, find_dotenv
+import os
+from pathlib import Path
+
+# @pytest.fixture(scope="session", autouse=True)
+# def load_env():
+#    env_file = find_dotenv(".env")
+#    load_dotenv(env_file)
 
 
 @pytest.fixture(scope="session", autouse=True)
-def load_env():
-    env_file = find_dotenv(".env")
-    load_dotenv(env_file)
+def set_env():
+    os.environ["YUCCA_PREPROCESSED"] = Path(__file__).parent.absolute() / "data"
+    os.environ["YUCCA_RAW_DATA"] = Path(__file__).parent.absolute() / "data"
+    os.environ["YUCCA_MODELS"] = Path(__file__).parent.absolute() / "data"
+    os.environ["YUCCA_RESULTS"] = Path(__file__).parent.absolute() / "data"
