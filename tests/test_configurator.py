7a1be079572b2cd10cf8147475a442b258fada73
LlambiasMBP <llambias@live.com>
2023-11-16 13:53:05 +0100

    v6

--- tests/test_configurator.py
+++ tests/test_configurator.py
@@ -7,16 +7,6 @@ def test_manager_works():
     print(yucca_preprocessed)
     print(os.path.isdir(yucca_preprocessed))
     print(os.listdir(yucca_preprocessed))
-    print(os.path.isdir("/home"))
-    print(os.path.isdir("/home/runner"))
-    print(os.path.isdir("/home/runner/work"))
-    print(os.path.isdir("/home/runner/work/yucca"))
-    print(os.path.isdir("/home/runner/work/yucca/tests"))
-    print(os.path.isdir("/home/runner/work/yucca/tests/data"))
-    print(os.listdir("/home/runner/work/yucca/tests/data"))
-    print(os.path.isdir("/home/runner/work/yucca/tests/data/Task000_Test"))
-    print(os.path.isdir("/home/runner/work/yucca/tests/data/Task000_Test/YuccaPlanner"))
-    print(os.getcwd())
 
     configurator = YuccaConfigurator(task="Task000_Test")
     assert configurator is not None
