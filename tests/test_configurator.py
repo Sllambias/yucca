32fbe1fa41424f4397e2f28d8dacc97bb03301a7
Sebastian <llambias@live.com>
2023-11-16 11:52:14 +0100

    fix2

--- tests/test_configurator.py
+++ tests/test_configurator.py
@@ -3,6 +3,8 @@ def test_manager_works():
     import os
     from yucca.training.trainers.YuccaConfigurator import YuccaConfigurator
 
+    print(os.getcwd())
+
     configurator = YuccaConfigurator(task="Task000_Test")
     assert configurator is not None
 
