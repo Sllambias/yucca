a713c05ec9cd3da5d753e2aa59d6bec1ef67686a
Asbjørn Munk <asbjorn@hey.com>
2023-11-20 17:00:53 +0100

    Make script to run task conversion

---
+++ yucca/run/run_task_conversion.py
@@ -0,0 +1,32 @@
+import argparse
+from yucca.paths import yucca_source
+import importlib
+import re
+
+
+def remove_task_prefix(str):
+    """
+    Removes the "TaskXXX_" prefix from string. Number is arbitrary, and T must be capitalized.
+    Example:
+      Input: "Task001_OASIS_IS_COOL"
+      Output: "OASIS_IS_COOL"
+    """
+    pattern = re.compile(r"^Task\d+_", re.IGNORECASE)
+    return pattern.sub("", str)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-t", "--task", help="Name of the task to preprocess. " "Should be of format: TaskXXX_MYTASK", required=True
+    )
+    parser.add_argument("-p", "--path", help="Path to raw data", default=yucca_source)
+    args = parser.parse_args()
+
+    task_name = remove_task_prefix(args.task)
+    task_converter = importlib.import_module(f"yucca.task_conversion.{args.task}")
+    task_converter.convert(f"{args.path}/{task_name}")
+
+
+if __name__ == "__main__":
+    main()
--- yucca/task_conversion/Task001_OASIS.py
+++ yucca/task_conversion/Task001_OASIS.py
@@ -29,79 +29,72 @@ Task_conversion file for a dataset with 1 modality.
     maybe_mkdir_p(join(yucca_preprocessing_dir, task_name))
     save_pickle(splits, join(yucca_preprocessing_dir, task_name, 'splits.pkl'))
 """
-import numpy as np
 from batchgenerators.utilities.file_and_folder_operations import join, maybe_mkdir_p, subfiles
 from yucca.task_conversion.utils import generate_dataset_json
+from yucca.utils.task_ids import maybe_get_task_from_task_id
 import shutil
 import gzip
 from yucca.paths import yucca_raw_data
-import argparse
 from tqdm import tqdm
 
-parser = argparse.ArgumentParser()
 
-parser.add_argument("-p", "--path", help="Path to OASIS dataset")
-parser.add_argument("--file_ext", default=".nii", help="Input file extension")
+def convert(path: str):
+    ###INPUT DATA###
 
-args = parser.parse_args()
+    file_suffix = ".nii"
 
-###INPUT DATA###
-# Input path and names
-base_in = args.path
-file_suffix = args.file_ext
+    # Train/Test Splits
+    labels_dir_tr = join(path, "Labels", "Train")
+    images_dir_tr = join(path, "Images", "Train")
+    training_samples = subfiles(labels_dir_tr, join=False, suffix=file_suffix)
 
-# Train/Test Splits
-labels_dir_tr = join(base_in, "Labels", "Train")
-images_dir_tr = join(base_in, "Images", "Train")
-training_samples = subfiles(labels_dir_tr, join=False, suffix=file_suffix)
+    labels_dir_ts = join(path, "Labels", "Test")
+    images_dir_ts = join(path, "Images", "Test")
+    test_samples = subfiles(labels_dir_ts, join=False, suffix=file_suffix)
 
-labels_dir_ts = join(base_in, "Labels", "Test")
-images_dir_ts = join(base_in, "Images", "Test")
-test_samples = subfiles(labels_dir_ts, join=False, suffix=file_suffix)
+    ###OUTPUT DATA
+    # Target names
+    task_name = "Task001_OASIS"
+    prefix = "OASIS"
 
-###OUTPUT DATA
-# Target names
-task_name = "Task001_OASIS"
-prefix = "OASIS"
+    # Target paths
+    target_base = join(yucca_raw_data, task_name)
 
-# Target paths
-target_base = join(yucca_raw_data, task_name)
+    target_imagesTr = join(target_base, "imagesTr")
+    target_labelsTr = join(target_base, "labelsTr")
 
-target_imagesTr = join(target_base, "imagesTr")
-target_labelsTr = join(target_base, "labelsTr")
+    target_imagesTs = join(target_base, "imagesTs")
+    target_labelsTs = join(target_base, "labelsTs")
 
-target_imagesTs = join(target_base, "imagesTs")
-target_labelsTs = join(target_base, "labelsTs")
+    maybe_mkdir_p(target_imagesTr)
+    maybe_mkdir_p(target_labelsTs)
+    maybe_mkdir_p(target_imagesTs)
+    maybe_mkdir_p(target_labelsTr)
 
-maybe_mkdir_p(target_imagesTr)
-maybe_mkdir_p(target_labelsTs)
-maybe_mkdir_p(target_imagesTs)
-maybe_mkdir_p(target_labelsTr)
+    ###Populate Target Directory###
+    # This is likely also the place to apply any re-orientation, resampling and/or label correction.
+    for sTr in tqdm(training_samples, desc="Train"):
+        serial_number = sTr[:-4]
+        image_file = open(join(images_dir_tr, sTr), "rb")
+        label = open(join(labels_dir_tr, sTr), "rb")
+        shutil.copyfileobj(image_file, gzip.open(f"{target_imagesTr}/{prefix}_{serial_number}_000.nii.gz", "wb"))
+        shutil.copyfileobj(label, gzip.open(f"{target_labelsTr}/{prefix}_{serial_number}.nii.gz", "wb"))
 
-###Populate Target Directory###
-# This is likely also the place to apply any re-orientation, resampling and/or label correction.
-for sTr in tqdm(training_samples, desc="Train"):
-    serial_number = sTr[:-4]
-    image_file = open(join(images_dir_tr, sTr), "rb")
-    label = open(join(labels_dir_tr, sTr), "rb")
-    shutil.copyfileobj(image_file, gzip.open(f"{target_imagesTr}/{prefix}_{serial_number}_000.nii.gz", "wb"))
-    shutil.copyfileobj(label, gzip.open(f"{target_labelsTr}/{prefix}_{serial_number}.nii.gz", "wb"))
+    for sTs in tqdm(test_samples, desc="Test"):
+        serial_number = sTs[:-4]
+        image_file = open(join(images_dir_ts, sTs), "rb")
+        label = open(join(labels_dir_ts, sTs), "rb")
+        shutil.copyfileobj(image_file, gzip.open(f"{target_imagesTs}/{prefix}_{serial_number}_000.nii.gz", "wb"))
+        shutil.copyfileobj(label, gzip.open(f"{target_labelsTs}/{prefix}_{serial_number}.nii.gz", "wb"))
 
-for sTs in tqdm(test_samples, desc="Test"):
-    serial_number = sTs[:-4]
-    image_file = open(join(images_dir_ts, sTs), "rb")
-    label = open(join(labels_dir_ts, sTs), "rb")
-    shutil.copyfileobj(image_file, gzip.open(f"{target_imagesTs}/{prefix}_{serial_number}_000.nii.gz", "wb"))
-    shutil.copyfileobj(label, gzip.open(f"{target_labelsTs}/{prefix}_{serial_number}.nii.gz", "wb"))
-
-generate_dataset_json(
-    join(target_base, "dataset.json"),
-    target_imagesTr,
-    target_imagesTs,
-    ("T1",),
-    labels={0: "background", 1: "Left Hippocampus", 2: "Right Hippocampus"},
-    dataset_name=task_name,
-    license="hands off!",
-    dataset_description="OASIS Dataset",
-    dataset_reference="",
-)
+    generate_dataset_json(
+        join(target_base, "dataset.json"),
+        target_imagesTr,
+        target_imagesTs,
+        ("T1",),
+        labels={0: "background", 1: "Left Hippocampus", 2: "Right Hippocampus"},
+        dataset_name=task_name,
+        license="hands off!",
+        dataset_description="OASIS Dataset",
+        dataset_reference="",
+    )
