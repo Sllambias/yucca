e0fd98cbfc14dee61d0f9e894d06e89aaaa03949
LlambiasMBP <llambias@live.com>
2023-11-20 10:55:12 +0100

    Fixed issues in comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,6 @@
 import pytest
-from dotenv import load_dotenv, find_dotenv
 import os
 from pathlib import Path
-
-# @pytest.fixture(scope="session", autouse=True)
-# def load_env():
-#    env_file = find_dotenv(".env")
-#    load_dotenv(env_file)
 
 
 @pytest.fixture(scope="session", autouse=True)
--- tests/test_configurator.py
+++ tests/test_configurator.py
@@ -1,15 +1,7 @@
-# %%
-def test_manager_works():
+def test_configurator_works():
     import os
     from yucca.training.trainers.YuccaConfigurator import YuccaConfigurator
     from yucca.paths import yucca_preprocessed
 
-    print(yucca_preprocessed)
-    print(os.path.isdir(yucca_preprocessed))
-    print(os.listdir(yucca_preprocessed))
-
     configurator = YuccaConfigurator(task="Task000_Test")
     assert configurator is not None
-
-
-# %%
