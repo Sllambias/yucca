fc818e73fe5d64a97a539c233db0d959d4dc7ca8
zcr545 <snl@di.ku.dk>
2023-11-28 15:29:22 +0100

    fix test

--- tests/test_training.py
+++ tests/test_training.py
@@ -1,4 +1,4 @@
-def test_manager_works():
+def test_training_works():
     from yucca.training.trainers.YuccaLightningManager import YuccaLightningManager
 
     manager = YuccaLightningManager(task="Task000_Test", fast_dev_run=True, disable_logging=True, model_name="TinyUNet")
