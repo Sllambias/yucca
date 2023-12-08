6a4a563c89713e82a397130331c78ec8928274fc
Asbjørn Munk <9844416+asbjrnmunk@users.noreply.github.com>
2023-12-08 16:43:06 +0100

    Configure batch size (#38)
    * Handle all cases of choosing batch and patch size
    
    * Bump patch version
    
    * Fix tests
    
    * Minor spelling fix
    
    * Add 2D training and make training on CPU to allow testing locally

--- tests/test_training.py
+++ tests/test_training.py
@@ -1,5 +1,28 @@
-def test_training_works():
+def test_2D_training():
     from yucca.training.trainers.YuccaLightningManager import YuccaLightningManager
 
-    manager = YuccaLightningManager(task="Task000_Test", fast_dev_run=True, disable_logging=True, model_name="TinyUNet")
+    manager = YuccaLightningManager(
+        task="Task000_Test",
+        fast_dev_run=True,
+        disable_logging=True,
+        model_name="TinyUNet",
+        model_dimensions="2D",
+        precision="bf16-mixed",
+        accelerator="cpu",
+    )
+    manager.run_training()
+
+
+def test_3D_training():
+    from yucca.training.trainers.YuccaLightningManager import YuccaLightningManager
+
+    manager = YuccaLightningManager(
+        task="Task000_Test",
+        fast_dev_run=True,
+        disable_logging=True,
+        model_name="TinyUNet",
+        model_dimensions="3D",
+        precision="bf16-mixed",
+        accelerator="cpu",
+    )
     manager.run_training()
