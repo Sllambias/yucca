692abb7e8822b1fee4fb93e2954a5d466198c4f6
LlambiasMBP <llambias@live.com>
2023-12-13 16:28:30 +0100

    now as dataclass and function

--- yucca/training/configuration/CallbackConfigurator.py
+++ yucca/training/configuration/CallbackConfigurator.py
@@ -1,84 +1,98 @@
 import numpy as np
+from typing import Union
+from dataclasses import dataclass
 from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.profilers import SimpleProfiler
 from pytorch_lightning.loggers import WandbLogger, CSVLogger
 from yuccalib.evaluation.loggers import YuccaLogger
 from yuccalib.utils.files_and_folders import WritePredictionFromLogits
+from lightning.pytorch.profilers.profiler import Profiler
 
 
+@dataclass
 class CallbackConfigurator:
-    def __init__(
-        self,
-        task: str,
-        outpath: str,
-        save_dir: str,
-        version: int,
-        checkpoint_interval: int = 250,
-        disable_logging: bool = False,
-        prediction_output_dir: str = None,
-        profile: bool = False,
-        save_softmax: bool = False,
-        steps_per_epoch: int = 250,
-    ):
-        self.checkpoint_interval = checkpoint_interval
-        self.disable_logging = disable_logging
-        self.outpath = outpath
-        self.prediction_output_dir = prediction_output_dir
-        self.profile = profile
-        self.task = task
-        self.save_dir = save_dir
-        self.save_softmax = save_softmax
-        self.steps_per_epoch = steps_per_epoch
-        self.version = version
-        self.setup_callbacks()
-        self.setup_loggers()
-        self.setup_profiler()
+    callbacks: list
+    loggers: list
+    profiler: Profiler
 
-    def setup_loggers(self):
-        # The YuccaLogger is the barebones logger needed to save hparams.yaml
-        # It should generally never be disabled.
-        self.loggers = []
-        self.loggers.append(
-            YuccaLogger(
-                disable_logging=self.disable_logging,
-                save_dir=self.save_dir,
-                name=None,
-                version=self.version,
-                steps_per_epoch=self.steps_per_epoch,
+
+def configure_callbacks(
+    task: str,
+    save_dir: str,
+    version_dir: str,
+    version: int,
+    checkpoint_interval: int = 250,
+    disable_logging: bool = False,
+    prediction_output_dir: str = None,
+    profile: bool = False,
+    save_softmax: bool = False,
+    steps_per_epoch: int = 250,
+):
+    callbacks = get_callbacks(checkpoint_interval, prediction_output_dir, save_softmax)
+    loggers = get_loggers(task, save_dir, version_dir, version, disable_logging, steps_per_epoch)
+    profiler = get_profiler(profile, save_dir)
+
+    return CallbackConfigurator(callbacks=callbacks, loggers=loggers, profiler=profiler)
+
+
+def get_loggers(
+    task: str,
+    save_dir: str,
+    version_dir: str,
+    version: Union[int, str],
+    disable_logging: bool = False,
+    steps_per_epoch: int = 250,
+):
+    # The YuccaLogger is the barebones logger needed to save hparams.yaml
+    # It should generally never be disabled.
+    if isinstance(version, str):
+        version = int(version)
+
+    loggers = [
+        YuccaLogger(
+            disable_logging=disable_logging,
+            save_dir=version_dir,
+            name=None,
+            version=version,
+            steps_per_epoch=steps_per_epoch,
+        )
+    ]
+    if not disable_logging:
+        loggers.append(
+            WandbLogger(
+                name=f"version_{version}",
+                save_dir=save_dir,
+                version=str(version),
+                project="Yucca",
+                group=task,
+                log_model="all",
             )
         )
-        if not self.disable_logging:
-            self.loggers.append(
-                WandbLogger(
-                    name=f"version_{self.version}",
-                    save_dir=self.outpath,
-                    version=str(self.version),
-                    project="Yucca",
-                    group=self.task,
-                    log_model="all",
-                )
-            )
+    return loggers
 
-    def setup_callbacks(self):
-        best_ckpt = ModelCheckpoint(
-            monitor="val_loss", mode="min", save_top_k=1, filename="best", enable_version_counter=False
-        )
-        interval_ckpt = ModelCheckpoint(
-            every_n_epochs=self.checkpoint_interval, save_top_k=-1, filename="{epoch}", enable_version_counter=False
-        )
-        latest_ckpt = ModelCheckpoint(
-            every_n_epochs=int(np.ceil(self.checkpoint_interval / 10)),
-            save_top_k=1,
-            filename="last",
-            enable_version_counter=False,
-        )
-        pred_writer = WritePredictionFromLogits(
-            output_dir=self.prediction_output_dir, save_softmax=self.save_softmax, write_interval="batch"
-        )
-        self.callbacks = [best_ckpt, interval_ckpt, latest_ckpt, pred_writer]
 
-    def setup_profiler(self):
-        if self.profile:
-            self.profiler = SimpleProfiler(dirpath=self.outpath, filename="simple_profile")
-        else:
-            self.profiler = None
+def get_callbacks(checkpoint_interval, prediction_output_dir: str = None, save_softmax: bool = False):
+    best_ckpt = ModelCheckpoint(monitor="val_loss", mode="min", save_top_k=1, filename="best", enable_version_counter=False)
+    interval_ckpt = ModelCheckpoint(
+        every_n_epochs=checkpoint_interval, save_top_k=-1, filename="{epoch}", enable_version_counter=False
+    )
+    latest_ckpt = ModelCheckpoint(
+        every_n_epochs=int(np.ceil(checkpoint_interval / 10)),
+        save_top_k=1,
+        filename="last",
+        enable_version_counter=False,
+    )
+    pred_writer = WritePredictionFromLogits(
+        output_dir=prediction_output_dir, save_softmax=save_softmax, write_interval="batch"
+    )
+    return [best_ckpt, interval_ckpt, latest_ckpt, pred_writer]
+
+
+def get_profiler(profile, outpath):
+    if profile:
+        return SimpleProfiler(dirpath=outpath, filename="simple_profile")
+    else:
+        return None
+
+
+callback_config = configure_callbacks(task="Task001", version_dir=".", save_dir=".", version=0, profile=False)
