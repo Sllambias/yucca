7068c5099a0a99db10943221e7353293d5333dc2
Drilip <gks462@alumni.ku.dk>
2024-01-16 15:46:19 +0100

    augmentation params can now be overwritten in the manager.

--- yucca/training/managers/YuccaManager.py
+++ yucca/training/managers/YuccaManager.py
@@ -56,6 +56,7 @@ class YuccaManager:
         model_name: str = "TinyUNet",
         num_workers: int = 8,
         patch_size: Union[tuple, Literal["max", "min", "mean"]] = None,
+        augmentation_params: dict = {},
         planner: str = "YuccaPlanner",
         precision: str = "16-mixed",
         profile: bool = False,
@@ -78,6 +79,7 @@ class YuccaManager:
         self.model_name = model_name
         self.name = self.__class__.__name__
         self.num_workers = num_workers
+        self.augmentation_params = augmentation_params
         self.planner = planner
         self.precision = precision
         self.profile = profile
@@ -150,6 +152,7 @@ class YuccaManager:
             deep_supervision=self.deep_supervision,
             patch_size=input_dims_config.patch_size,
             is_2D=True if self.model_dimensions == "2D" else False,
+            parameter_dict=self.augmentation_params,
             task_type_preset=plan_config.task_type,
         )
 
