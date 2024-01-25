39df49bc0628bdccd65ebe5439baa5a1a23d16ac
Drilip <gks462@alumni.ku.dk>
2024-01-25 13:30:30 +0100

    Black formatting

--- yucca/task_conversion/Task006_WMH_Flair.py
+++ yucca/task_conversion/Task006_WMH_Flair.py
@@ -18,7 +18,6 @@ def convert(path: str, subdir: str = "WMH"):
     datasets = ["Amsterdam", "Singapore", "Utrecht"]
     site = ""
 
-
     # Target paths
     target_base = join(yucca_raw_data, task_name)
 
@@ -36,13 +35,13 @@ def convert(path: str, subdir: str = "WMH"):
     ###Populate Target Directory###
     for dataset in datasets:
         dataset_path = join(path, dataset)
-        if dataset == 'Amsterdam':
-            tr_folder = 'Train_GE3T'
+        if dataset == "Amsterdam":
+            tr_folder = "Train_GE3T"
         else:
-            tr_folder = 'Train'
+            tr_folder = "Train"
 
         train_folder = join(dataset_path, tr_folder)
-        test_folder = join(dataset_path, 'Test')
+        test_folder = join(dataset_path, "Test")
 
         training_samples = subfolders(train_folder, join=False)
         test_samples = subfolders(test_folder, join=False)
@@ -50,29 +49,35 @@ def convert(path: str, subdir: str = "WMH"):
         # First we sort the training data
         for sTr in training_samples:
             # Loading relevant modalities and the ground truth
-            image_file = open(join(train_folder, sTr, 'pre', 'FLAIR.nii.gz'), "rb")
-            label = open(join(train_folder, sTr, 'pre', 'wmh.nii.gz'), "rb")
+            image_file = open(join(train_folder, sTr, "pre", "FLAIR.nii.gz"), "rb")
+            label = open(join(train_folder, sTr, "pre", "wmh.nii.gz"), "rb")
             shutil.copyfileobj(image_file, gzip.open(f"{target_imagesTr}/{task_prefix}_{sTr}_000.nii.gz", "wb"))
             shutil.copyfileobj(label, gzip.open(f"{target_labelsTr}/{task_prefix}_{sTr}.nii.gz", "wb"))
 
         # Now we sort the test data
-        if dataset == 'Amsterdam':
+        if dataset == "Amsterdam":
             for site in test_samples:
                 samples = subfolders(join(test_folder, site), join=False)
                 for sTs in samples:
-                    image_file = open(join(test_folder, site, sTs, 'pre', 'FLAIR.nii.gz'), "rb")
-                    label = open(join(test_folder, site, sTs, 'pre', 'wmh.nii.gz'), "rb")
+                    image_file = open(join(test_folder, site, sTs, "pre", "FLAIR.nii.gz"), "rb")
+                    label = open(join(test_folder, site, sTs, "pre", "wmh.nii.gz"), "rb")
                     shutil.copyfileobj(image_file, gzip.open(f"{target_imagesTs}/{task_prefix}_{sTs}_000.nii.gz", "wb"))
                     shutil.copyfileobj(label, gzip.open(f"{target_labelsTs}/{task_prefix}_{sTs}.nii.gz", "wb"))
         else:
             for sTs in test_samples:
-                image_file = open(join(test_folder, sTs, 'pre', 'FLAIR.nii.gz'), "rb")
-                label = open(join(test_folder, sTs, 'pre', 'wmh.nii.gz'), "rb")
+                image_file = open(join(test_folder, sTs, "pre", "FLAIR.nii.gz"), "rb")
+                label = open(join(test_folder, sTs, "pre", "wmh.nii.gz"), "rb")
                 shutil.copyfileobj(image_file, gzip.open(f"{target_imagesTs}/{task_prefix}_{sTs}_000.nii.gz", "wb"))
                 shutil.copyfileobj(label, gzip.open(f"{target_labelsTs}/{task_prefix}_{sTs}.nii.gz", "wb"))
 
-    generate_dataset_json(join(target_base, 'dataset.json'), target_imagesTr, target_imagesTs, ('Flair'),
-                        labels={0: 'background', 1: 'WMH', 2: 'Other Pathology'},
-                        dataset_name=task_name, license='CC BY-NC 4.0 DEED',
-                        dataset_description="White Matter Hyperintensity Segmentation Challenge. Flair images only!",
-                        dataset_reference="https://dataverse.nl/dataset.xhtml?persistentId=doi:10.34894/AECRSD")
+    generate_dataset_json(
+        join(target_base, "dataset.json"),
+        target_imagesTr,
+        target_imagesTs,
+        ("Flair"),
+        labels={0: "background", 1: "WMH", 2: "Other Pathology"},
+        dataset_name=task_name,
+        license="CC BY-NC 4.0 DEED",
+        dataset_description="White Matter Hyperintensity Segmentation Challenge. Flair images only!",
+        dataset_reference="https://dataverse.nl/dataset.xhtml?persistentId=doi:10.34894/AECRSD",
+    )
