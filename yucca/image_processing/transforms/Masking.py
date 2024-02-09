2b4fc98364de5d2ab7a54c0eae5ab09bc7407f1a
LlambiasMBP <llambias@live.com>
2024-02-09 11:35:16 +0100

    Black Formatting and linting

--- yucca/image_processing/transforms/Masking.py
+++ yucca/image_processing/transforms/Masking.py
@@ -1,13 +1,14 @@
 import numpy as np
 import logging
 from yucca.image_processing.transforms.YuccaTransform import YuccaTransform
-from typing import Union, Tuple, List, Optional, Iterable
+from typing import Union, Iterable
+
 
 class Masking(YuccaTransform):
     """
     A Yucca transform class for applying masking to input images or image volumes.
 
-    It can be used for masking with a fixed ratio and fixed token size but it can also be used 
+    It can be used for masking with a fixed ratio and fixed token size but it can also be used
     to mask a random ratio of each batch and with random token sizes.
 
     Args:
@@ -22,12 +23,18 @@ class Masking(YuccaTransform):
                 to the image volume size. For token size 0.05 with an image of shape [200, 100, 160] mask_token_size will be
                 [200*0.05, 100*0.05, 160*0.05] = [10, 5, 8].
             If mask_token_size is given as an iterator of ints (e.g. [16,16,16]) it will remain unchanged.
-            if mask_token_size is given as an iterator of two floats they will be uniformly sampled per batch, and then
+            If mask_token_size is given as an iterator of two floats they will be uniformly sampled per batch, and then
                 otherwise treated as the above case with a single float.
 
     """
 
-    def __init__(self, mask=False, data_key="image", mask_ratio: Union[Iterable[float], float] = 0.25, mask_token_size: Union[Iterable[Union[int, float]], float, int] = 0.05):
+    def __init__(
+        self,
+        mask=False,
+        data_key="image",
+        mask_ratio: Union[Iterable[float], float] = 0.25,
+        mask_token_size: Union[Iterable[Union[int, float]], float, int] = 0.05,
+    ):
         self.mask = mask
         self.data_key = data_key
         self.mask_ratio = mask_ratio
@@ -43,21 +50,27 @@ class Masking(YuccaTransform):
             assert 1 > mask_token_size > 0, "if mask_token_size is a float it needs to be between 1 and 0."
             " It will be treated as a ratio to mask for each image dimension."
             if mask_token_size > 0.25:
-                logging.warn("mask_token_size is set to a ratio over 0.25 of the image. "
-                             "This is not intended and should be reconsidered.")
+                logging.warn(
+                    "mask_token_size is set to a ratio over 0.25 of the image. "
+                    "This is not intended and should be reconsidered."
+                )
             mask_token_size = [int(i * mask_token_size) for i in input_shape]
             print(mask_token_size)
         elif isinstance(mask_token_size, int):
             mask_token_size = [mask_token_size for i in input_shape]
         elif isinstance(mask_token_size, (tuple, list)) and isinstance(mask_token_size[0], float):
-            # If we get a 
-            assert len(mask_token_size) == 2 and np.all(1 > np.array(mask_token_size)) and np.all(np.array(mask_token_size) > 0)
+            # If we get a
+            assert (
+                len(mask_token_size) == 2 and np.all(1 > np.array(mask_token_size)) and np.all(np.array(mask_token_size) > 0)
+            )
             if np.any(np.array(mask_token_size) > 0.25):
-                logging.warn("mask_token_size is set to a ratio over 0.25 of the image. "
-                             "This is not intended and should be reconsidered.")
+                logging.warn(
+                    "mask_token_size is set to a ratio over 0.25 of the image. "
+                    "This is not intended and should be reconsidered."
+                )
             mask_token_size = [int(i * np.random.uniform(*mask_token_size)) for i in input_shape]
         return mask_ratio, mask_token_size
-    
+
     def __mask__(self, image_volume, mask_ratio, mask_token_size):
         assert len(image_volume.shape[2:]) == len(mask_token_size), (
             "mask token size not compatible with input data"
@@ -74,7 +87,7 @@ class Masking(YuccaTransform):
         grid = grid_flat.reshape(grid_dims)
         for idx, size in enumerate(mask_token_size):
             grid = np.repeat(grid, repeats=size, axis=idx)
-        
+
         image_volume = np.multiply(image_volume, grid[*slices])
         return image_volume
 
@@ -85,7 +98,8 @@ class Masking(YuccaTransform):
         ), f"Incorrect data size or shape.\
             \nShould be (b, c, x, y, z) or (b, c, x, y) and is: {data_dict[self.data_key].shape}"
         if self.mask:
-            mask_ratio, mask_token_size= self.get_params(data_dict[self.data_key].shape[2:], self.mask_ratio, self.mask_token_size)
+            mask_ratio, mask_token_size = self.get_params(
+                data_dict[self.data_key].shape[2:], self.mask_ratio, self.mask_token_size
+            )
             data_dict[self.data_key] = self.__mask__(data_dict[self.data_key], mask_ratio, mask_token_size)
         return data_dict
-
