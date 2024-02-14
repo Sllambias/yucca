76e23ef6208420787f61abbcb3b16c0e6669a843
Asbjørn Munk <9844416+asbjrnmunk@users.noreply.github.com>
2024-02-14 17:13:59 +0100

    Add ADNI tc

---
+++ yucca/task_conversion/Task207_ADNI.py
@@ -0,0 +1,56 @@
+import gzip
+from batchgenerators.utilities.file_and_folder_operations import join, maybe_mkdir_p, subfiles
+from yucca.task_conversion.utils import generate_dataset_json, dirs_in_dir
+from yucca.paths import yucca_raw_data
+from tqdm import tqdm
+import shutil
+
+
+def convert(path: str, subdir: str = "ADNI_NIFTI"):
+    """INPUT DATA - Define input path and suffixes"""
+    path = join(path, subdir)
+    ext = ".nii"
+
+    """ OUTPUT DATA - Define the task name and prefix """
+    task_name = "Task207_ADNI"
+    task_prefix = "ADNI"
+
+    """ Access the input data. If images are not split into train/test, and you wish to randomly
+    split the data, uncomment and adapt the following lines to fit your local path. """
+
+    subjects_dir = join(path, "ADNI")
+    target_base = join(yucca_raw_data, task_name)
+    target_imagesTr = join(target_base, "imagesTr")
+
+    maybe_mkdir_p(target_imagesTr)
+
+    """Populate Target Directory
+    This is also the place to apply any re-orientation, resampling and/or label correction."""
+
+    for subject in tqdm(dirs_in_dir(subjects_dir), desc="Subjects"):
+        subject_dir = join(subjects_dir, subject)
+        for modality in dirs_in_dir(subject_dir):
+            modality_dir = join(subject_dir, modality)
+            for session in dirs_in_dir(modality_dir):
+                session_dir = join(modality_dir, session)
+                for id in dirs_in_dir(session_dir):
+                    id_dir = join(session_dir, id)
+                    for file in subfiles(id_dir, join=False, suffix=ext):
+                        image_path = join(id_dir, file)
+                        file_name = file[: -len(ext)]
+                        output_name = f"{task_prefix}_{file_name}_000.nii.gz"
+                        output_path = join(target_imagesTr, output_name)
+                        image_file = open(image_path, "rb")
+                        shutil.copyfileobj(image_file, gzip.open(output_path, "wb"))
+
+    generate_dataset_json(
+        join(target_base, "dataset.json"),
+        target_imagesTr,
+        imagesTs_dir=None,
+        modalities=["MRI"],
+        labels=None,
+        dataset_name=task_name,
+        license="CC-BY-SA 4.0",
+        dataset_description="Decathlon: Brain Tumour Segmentation",
+        dataset_reference="King's College London",
+    )
