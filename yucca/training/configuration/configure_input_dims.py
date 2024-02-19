d2922cda100317ace39a406ea8fd66e9c6139dde
zcr545 <snl@di.ku.dk>
2024-02-19 14:35:42 +0100

    add ckpt_ps

--- yucca/training/configuration/configure_input_dims.py
+++ yucca/training/configuration/configure_input_dims.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Tuple, Union, Optional, Literal
+from typing import Tuple, Union, Optional, Literal, Iterable
 from yucca.network_architectures.utils.model_memory_estimation import find_optimal_tensor_dims
 import logging
 
@@ -23,33 +23,35 @@ def get_input_dims_config(
     model_dimensions: Literal["2D", "3D"],
     model_name: str,
     num_classes: int,
+    ckpt_patch_size: Optional[Iterable[int]] = None,
     max_vram: Optional[int] = None,
     batch_size: Union[int, Literal["tiny"]] = None,
     patch_based_training: bool = True,
     patch_size: Union[Literal["max", "min", "mean", "tiny"], Tuple[int, int], Tuple[int, int, int], None] = None,
 ):
+    """
+    The priority list for setting patch_size:
+    1. Patch based training is false = we train on the full image size of the dataset.
+        - This requires the dataset to be preprocessed with a planner that uses "fixed_target_size" such as
+        the /planning/resampling/YuccaPlanner_MaxSize.py
+    2. Patch size is set in the manager. This overrides other defaults and inferred patch sizes.
+    3. Patch size is set in the ckpt.
+        - Relevant for finetuning and inference. This happens if patch size was originally set using the manager.
+        In this case we have patch size X from the manager and "default" patch size Y in the plans, where X
+        originally overruled Y and thus should do so again.
+    4. Patch size is inferred in the plans.
+    """
+
     num_modalities = max(1, plan.get("num_modalities") or len(plan["dataset_properties"]["modalities"]))
 
+    # Check patch size priority 1
     if patch_based_training is False:
         assert plan.get("new_max_size") == plan.get(
             "new_min_size"
         ), "sizes in dataset are not uniform. Non-patch based training only works for datasets with uniform data shapes."
         patch_size = tuple(plan.get("new_max_size"))
 
-    # If batch_size or patch_size is not provided, try to infer it from the plan
-    if batch_size is None and plan.get("batch_size"):
-        batch_size = plan.get("batch_size")
-    if patch_size is None and plan.get("patch_size"):
-        patch_size = tuple(plan.get("patch_size"))
-
-    # onvert batch_size provided as string to int
-    if isinstance(batch_size, str):
-        if batch_size == "tiny":
-            batch_size = 2
-        else:
-            raise ValueError(f"Unknown batch size param: {batch_size}")
-
-    # convert patch_size provided as string to tuple
+    # Check patch size priority 2
     if isinstance(patch_size, str):
         if patch_size in ["max", "min", "mean"]:
             # Get patch size from dataset
@@ -59,11 +61,22 @@ def get_input_dims_config(
         else:
             raise ValueError(f"Unknown patch size param: {patch_size}")
 
-        if model_dimensions == "2D" and len(patch_size) == 3:
-            # If we have now selected a 3D patch for a 2D model we skip the first dim
-            # as we will be extracting patches from that dimension.
-            patch_size = patch_size[1:]
+    # Patch size priority 3
+    if ckpt_patch_size is not None and patch_size is None:
+        patch_size = ckpt_patch_size
 
+    if model_dimensions == "2D" and len(patch_size) == 3:
+        # If we have now selected a 3D patch for a 2D model we skip the first dim
+        # as we will be extracting patches from that dimension.
+        patch_size = patch_size[1:]
+
+    if isinstance(batch_size, str):
+        if batch_size == "tiny":
+            batch_size = 2
+        else:
+            raise ValueError(f"Unknown batch size param: {batch_size}")
+
+    # Patch size priority 4
     if patch_size is None or batch_size is None:
         batch_size, patch_size = find_optimal_tensor_dims(
             fixed_patch_size=patch_size,
