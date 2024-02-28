a4b43e0e13e6dd7c1064b680fbc616e25b478272
Asbjørn Munk <9844416+asbjrnmunk@users.noreply.github.com>
2024-02-28 14:12:11 +0100

    Remove unused import

--- yucca/training/data_loading/YuccaDataModule.py
+++ yucca/training/data_loading/YuccaDataModule.py
@@ -7,7 +7,6 @@ from torch.utils.data import DataLoader, Sampler
 from batchgenerators.utilities.file_and_folder_operations import join
 from yucca.training.configuration.configure_input_dims import InputDimensionsConfig
 from yucca.training.configuration.split_data import SplitConfig
-from yucca.training.configuration.configure_plans import PlanConfig
 from yucca.training.data_loading.YuccaDataset import YuccaTestDataset, YuccaTrainDataset
 from yucca.training.data_loading.samplers import InfiniteRandomSampler
 
