f2e8f44d74f1e6bccf5faae2b6c0e9f3a3ff6d56
zcr545 <snl@di.ku.dk>
2024-04-10 10:02:33 +0200

    Final placement of print statement

--- yucca/training/managers/augmentation/YuccaManager_PreserveRange.py
+++ yucca/training/managers/augmentation/YuccaManager_PreserveRange.py
@@ -8,6 +8,8 @@ class YuccaManager_PreserveRange(YuccaManager):
 
 
 if __name__ == "__main__":
+    # Test that shows the difference between no clipping and clipping to input range.
+    # Relevant for models trained on very strict ranges.
     man = YuccaManager_PreserveRange(
         task="Task001_OASIS",
         planner="YuccaPlanner_1_1_1",
@@ -19,7 +21,7 @@ if __name__ == "__main__":
     man.initialize(stage="fit")
     man.data_module.setup(stage="fit")
     dataset = iter(man.data_module.train_dataloader())
-    print("All samples start in the range 0-1")
+    print("All samples start in the range 0-1. \n Testing the YuccaManager_PreserveRange")
     for i in range(20):
         sample = next(dataset)
         print(f'Sample {i} min value: {sample["image"].min()} and max value: {sample["image"].max()}')
