528dcab095e279f858bc3899de3bf01c37bc3ecd
Asbjørn Munk <9844416+asbjrnmunk@users.noreply.github.com>
2024-04-22 11:12:44 +0200

    Fix lint

--- yucca/task_conversion/Task203_OASIS3.py
+++ yucca/task_conversion/Task203_OASIS3.py
@@ -1,4 +1,3 @@
-from re import sub
 import shutil
 from batchgenerators.utilities.file_and_folder_operations import join, maybe_mkdir_p, subfiles
 from yucca.task_conversion.utils import generate_dataset_json, dirs_in_dir, should_use_volume
