01c4c0c6d3f7edaa5a219caafe62772708676961
zcr545 <snl@di.ku.dk>
2024-05-13 10:52:52 +0200

    update sampler

--- yucca/data/samplers.py
+++ yucca/data/samplers.py
@@ -37,3 +37,22 @@ def _infinite_generator(n, rng):
     """Inifinitely returns a number in [0, n)."""
     while True:
         yield from torch.randperm(n, generator=rng).tolist()
+
+
+class InfiniteRandomSamplerWithOversampling(InfiniteRandomSampler):
+    """Return random indices from [0-n) infinitely.
+
+    Arguments:
+        dset_size (int): Size of the dataset to sample.
+    """
+
+    def __init__(self, dataset: torch.utils.data.Dataset, p_oversample_foreground):
+        self.dset_size = len(dataset)
+        self.p_oversample_foreground = p_oversample_foreground
+
+    def __iter__(self):
+        # Create a random number generator (optional, makes the sampling independent of the base RNG)
+        rng = torch.Generator()
+        seed = torch.empty((), dtype=torch.int64).random_().item()
+        rng.manual_seed(seed)
+        return _infinite_generator(self.dset_size, rng)
