b19175658e3250ffa4f4d226b067c7ba306fb9de
zcr545 <snl@di.ku.dk>
2024-07-22 10:20:36 +0200

    prep decath

--- yucca/pipeline/evaluation/challenge_preparation_scripts/prepare_decathlon.py
+++ yucca/pipeline/evaluation/challenge_preparation_scripts/prepare_decathlon.py
@@ -1,30 +1,33 @@
+# %%
 from batchgenerators.utilities.file_and_folder_operations import subfiles, join, maybe_mkdir_p
+from yucca.paths import yucca_results
 import shutil
 
 """
-Task031_
-Task032_
-Task033_
-Task034_
-Task035_
-Task036_
-Task037_
-Task038_
-Task039_
-Task040_
+Task031_MSD_BrainTumour
+Task032_MSD_Heart
+Task033_MSD_Liver
+Task034_MSD_Hippocampus
+Task035_MSD_Prostate
+Task036_MSD_Lung
+Task037_MSD_Pancreas
+Task038_MSD_HepaticVessel
+Task039_MSD_Spleen
+Task040_MSD_Colon
 """
 
-prefixes = [
-    "DC_BrainTumour_",
-    "DC_Heart_",
-    "DC_Liver_",
-    "DC_Hippocampus_",
-    "DC_Prostate_",
-    "DC_Lung_",
-    "DC_Pancreas_",
-    "DC_HepaticVessel_",
-    "DC_Spleen_",
-    "DC_Colon_",
+
+current = [
+    "Task031_MSD_BrainTumour",
+    "Task032_MSD_Heart",
+    "Task033_MSD_Liver",
+    "Task034_MSD_Hippocampus",
+    "Task035_MSD_Prostate",
+    "Task036_MSD_Lung",
+    "Task037_MSD_Pancreas",
+    "Task038_MSD_HepaticVessel",
+    "Task039_MSD_Spleen",
+    "Task040_MSD_Colon",
 ]
 
 expected = [
@@ -41,19 +44,19 @@ expected = [
 ]
 
 folders = [
-    "/home/zcr545/YuccaData/yucca_predictions/Task031_DC_BrainTumour/Task031_DC_BrainTumour/UNet3D/YuccaTrainerV2__YuccaPlannerV2/fold_0_checkpoint_best",
-    "/home/zcr545/YuccaData/yucca_predictions/Task032_DC_Heart/Task032_DC_Heart/UNet3D/YuccaTrainerV2__YuccaPlannerV2/fold_0_checkpoint_best",
-    "/home/zcr545/YuccaData/yucca_predictions/Task033_DC_Liver/Task033_DC_Liver/UNet3D/YuccaTrainerV2__YuccaPlannerV2/fold_0_checkpoint_best",
-    "/home/zcr545/YuccaData/yucca_predictions/Task034_DC_Hippocampus/Task034_DC_Hippocampus/UNet3D/YuccaTrainerV2__YuccaPlannerV2/fold_0_checkpoint_best",
-    "/home/zcr545/YuccaData/yucca_predictions/Task035_DC_Prostate/Task035_DC_Prostate/UNet3D/YuccaTrainerV2__YuccaPlannerV2/fold_0_checkpoint_best",
-    "/home/zcr545/YuccaData/yucca_predictions/Task036_DC_Lung/Task036_DC_Lung/UNet3D/YuccaTrainerV2__YuccaPlannerV2/fold_0_checkpoint_best",
-    "/home/zcr545/YuccaData/yucca_predictions/Task037_DC_Pancreas/Task037_DC_Pancreas/UNet3D/YuccaTrainerV2__YuccaPlannerV2/fold_0_checkpoint_best",
-    "/home/zcr545/YuccaData/yucca_predictions/Task038_DC_HepaticVessel/Task038_DC_HepaticVessel/UNet3D/YuccaTrainerV2__YuccaPlannerV2/fold_0_checkpoint_best",
-    "/home/zcr545/YuccaData/yucca_predictions/Task039_DC_Spleen/Task039_DC_Spleen/UNet3D/YuccaTrainerV2__YuccaPlannerV2/fold_0_checkpoint_best",
-    "/home/zcr545/YuccaData/yucca_predictions/Task040_DC_Colon/Task040_DC_Colon/UNet3D/YuccaTrainerV2__YuccaPlannerV2/fold_0_checkpoint_best",
+    "/projects/zcr545_shared_datasets/people/zcr545/YuccaData/yucca_results/Task031_MSD_BrainTumour/Task031_MSD_BrainTumour/UNet__3D/YuccaManagerV11__YuccaPlannerV3/default/kfold_5_fold_4/version_0/best",
+    "/projects/zcr545_shared_datasets/people/zcr545/YuccaData/yucca_results/Task032_MSD_Heart/Task032_MSD_Heart/UNet__3D/YuccaManagerV11__YuccaPlannerV3/default/kfold_5_fold_4/version_0/best",
+    "/projects/zcr545_shared_datasets/people/zcr545/YuccaData/yucca_results/Task033_MSD_Liver/Task033_MSD_Liver/UNet__3D/YuccaManagerV11__YuccaPlannerV3/default/kfold_5_fold_4/version_0/best",
+    "/projects/zcr545_shared_datasets/people/zcr545/YuccaData/yucca_results/Task034_MSD_Hippocampus/Task034_MSD_Hippocampus/UNet__3D/YuccaManagerV11__YuccaPlannerV3/default/kfold_5_fold_4/version_0/best",
+    "/projects/zcr545_shared_datasets/people/zcr545/YuccaData/yucca_results/Task035_MSD_Prostate/Task035_MSD_Prostate/UNet__3D/YuccaManagerV11__YuccaPlannerV3/default/kfold_5_fold_3/version_0/best",
+    "/projects/zcr545_shared_datasets/people/zcr545/YuccaData/yucca_results/Task036_MSD_Lung/Task036_MSD_Lung/UNet__3D/YuccaManagerV11__YuccaPlannerV3/default/kfold_5_fold_4/version_0/best",
+    "/projects/zcr545_shared_datasets/people/zcr545/YuccaData/yucca_results/Task037_MSD_Pancreas/Task037_MSD_Pancreas/UNet__3D/YuccaManagerV11__YuccaPlannerV3/default/kfold_5_fold_4/version_0/best",
+    "/projects/zcr545_shared_datasets/people/zcr545/YuccaData/yucca_results/Task038_MSD_HepaticVessel/Task038_MSD_HepaticVessel/UNet__3D/YuccaManagerV11__YuccaPlannerV3/default/kfold_5_fold_4/version_0/best",
+    "/projects/zcr545_shared_datasets/people/zcr545/YuccaData/yucca_results/Task039_MSD_Spleen/Task039_MSD_Spleen/UNet__3D/YuccaManagerV11__YuccaPlannerV3/default/kfold_5_fold_4/version_0/best",
+    "/projects/zcr545_shared_datasets/people/zcr545/YuccaData/yucca_results/Task040_MSD_Colon/Task040_MSD_Colon/UNet__3D/YuccaManagerV11__YuccaPlannerV3/default/kfold_5_fold_4/version_0/best",
 ]
 
-blacklist = [
+exclude = [
     "Task03_Liver/liver_141.nii.gz",
     "Task03_Liver/liver_156.nii.gz",
     "Task03_Liver/liver_160.nii.gz",
@@ -72,11 +75,52 @@ maybe_mkdir_p(savepath)
 
 for i in range(10):
     for seg_path in subfiles(folders[i], join=False):
-        new_filename = seg_path[len(prefixes[i]) :]
-        new_path = join(expected[i], new_filename)
-        if new_path in blacklist:
+        new_path = join(expected[i], seg_path)
+        if new_path in exclude:
             print("blocking: ", new_path)
             continue
         maybe_mkdir_p(join(savepath, expected[i]))
 
-        shutil.copy(join(folders[i], seg_path), f"{savepath}/{expected[i]}/{new_filename}")
+        shutil.copy2(join(folders[i], seg_path), f"{savepath}/{expected[i]}/{seg_path}")
+
+# %%
+
+# expected = [
+#    "Task01_BrainTumour",
+#    "Task02_Heart",
+#    "Task03_Liver",
+#    "Task04_Hippocampus",
+#    "Task05_Prostate",
+#    "Task06_Lung",
+#    "Task07_Pancreas",
+#    "Task08_HepaticVessel",
+#    "Task09_Spleen",
+#    "Task10_Colon",
+# ]
+#
+# from SimpleITK import ReadImage
+# from batchgenerators.utilities.file_and_folder_operations import subdirs
+# import glob
+# import os
+# import logging
+# from yucca.paths import yucca_raw_data
+#
+# logger = logging.getLogger()
+# sub_dir = "/home/zcr545/decathlon_submission"
+# gt_dir = "/home/zcr545/data/data/public_datasets/decathlon"
+## the path to your submit file
+## for d in subdirs(submit_file):
+# for i in range(10):
+#    if i in [0, 2, 7]:
+#        continue
+#    img_list = [x for x in glob.glob(os.path.join(gt_dir, expected[i], "imagesTs", "*.nii.gz"))]
+#    for path in img_list:
+#        img = ReadImage(path)
+#        name = os.path.split(path)[-1]
+#        gtpath = os.path.join(sub_dir, expected[i], name)
+#        gt = ReadImage(gtpath)
+#        if not img.GetOrigin() == gt.GetOrigin():
+#            print(path, img.GetOrigin(), gt.GetOrigin())
+#        if not img.GetSpacing() == gt.GetSpacing():
+#            print(path, img.GetSpacing(), gt.GetSpacing())
+# %%
