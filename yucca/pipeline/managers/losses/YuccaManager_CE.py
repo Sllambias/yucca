7019fd7e1759769619adb5d4eb1a34725ed58424
LlambiasMBP <llambias@live.com>
2024-08-02 15:25:50 +0200

    create templates

--- yucca/pipeline/managers/losses/YuccaManager_CE.py
+++ yucca/pipeline/managers/losses/YuccaManager_CE.py
@@ -1,19 +1,7 @@
 from yucca.pipeline.managers.YuccaManager import YuccaManager
-from yucca.optimization.loss_functions.CE import CE
 
 
 class YuccaManager_CE(YuccaManager):
-    def __init__(
-        self,
-        model,
-        model_dimensions: str,
-        task: str,
-        folds: str | int,
-        plan_id: str,
-        starting_lr: float = None,
-        loss_fn: str = None,
-        momentum: float = None,
-        continue_training: bool = False,
-    ):
-        super().__init__(model, model_dimensions, task, folds, plan_id, starting_lr, loss_fn, momentum, continue_training)
-        self.loss = CE
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.loss = "CE"
