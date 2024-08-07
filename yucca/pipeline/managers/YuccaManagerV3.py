6b1c03b8ef6b688e8c81878a3c68d50fda020e81
Sllambias <llambias@live.com>
2024-08-07 15:10:53 +0200

    .

--- yucca/pipeline/managers/YuccaManagerV3.py
+++ yucca/pipeline/managers/YuccaManagerV3.py
@@ -3,7 +3,7 @@ from yucca.pipeline.managers.YuccaManager import YuccaManager
 from yucca.data.augmentation.augmentation_presets import generic
 
 
-class YuccaManagerV11(YuccaManager):
+class YuccaManagerV3(YuccaManager):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.deep_supervision = True
@@ -11,5 +11,5 @@ class YuccaManagerV11(YuccaManager):
         self.augmentation_params["clip_to_input_range"] = True
         self.momentum = 0.99
         self.optimizer = torch.optim.AdamW
-        self.optim_kwargs = {"eps": 1e-8, "betas": (0.9, 0.99), "lr": 1e-4, "weight_decay": 5e-2}
+        self.optim_kwargs = {"eps": 1e-8, "betas": (0.9, 0.99), "lr": 5e-4, "weight_decay": 5e-2}
         self.p_oversample_foreground = 0.6
