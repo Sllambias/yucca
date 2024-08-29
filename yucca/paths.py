74ee7ca9324bca956e486d5f246b62658647522e
zcr545 <snl@di.ku.dk>
2024-08-29 10:29:32 +0200

    add planv6

--- yucca/paths.py
+++ yucca/paths.py
@@ -10,7 +10,7 @@ from batchgenerators.utilities.file_and_folder_operations import maybe_mkdir_p
 
 load_dotenv()
 
-vars = ["YUCCA_SOURCE", "YUCCA_RAW_DATA", "YUCCA_PREPROCESSED_DATA", "YUCCA_MODELS", "YUCCA_RESULTS"]
+vars = ["YUCCA_RAW_DATA", "YUCCA_PREPROCESSED_DATA", "YUCCA_MODELS", "YUCCA_RESULTS"]
 vals = {}
 
 for var in vars:
@@ -21,11 +21,15 @@ for var in vars:
         warnings.warn(f"Missing environment variable {var}.")
         vals[var] = None
 
-yucca_source = vals["YUCCA_SOURCE"]
 yucca_raw_data = vals["YUCCA_RAW_DATA"]
 yucca_preprocessed_data = vals["YUCCA_PREPROCESSED_DATA"]
 yucca_models = vals["YUCCA_MODELS"]
 yucca_results = vals["YUCCA_RESULTS"]
+
+if "YUCCA_SOURCE" in os.environ.keys():
+    yucca_source = os.environ["YUCCA_SOURCE"]
+else:
+    yucca_source = None
 
 if "YUCCA_WANDB_ENTITY" in os.environ.keys():
     yucca_wandb_entity = os.environ["YUCCA_WANDB_ENTITY"]
