3560f6d035eab4dfd12c761b685fdcb642a62739
Sllambias <llambias@live.com>
2024-09-09 15:23:17 +0200

    add better func paths

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,4 +1,6 @@
 import subprocess
+import os
+import yucca
 
 
 def test_task_convert():
@@ -6,12 +8,15 @@ def test_task_convert():
 
 
 def test_functional_preprocessing():
-    subprocess.run(["python", "yucca/documentation/templates/functional_preprocessing.py"], check=True)
+    print(yucca.__path__[0])
+    subprocess.run(
+        ["python", os.path.join(yucca.__path__[0], "documentation/templates/functional_preprocessing.py")], check=True
+    )
 
 
 def test_functional_training():
-    subprocess.run(["python", "yucca/documentation/templates/functional_training.py"], check=True)
+    subprocess.run(["python", os.path.join(yucca.__path__[0], "documentation/templates/functional_training.py")], check=True)
 
 
 def test_functional_inference():
-    subprocess.run(["python", "yucca/documentation/templates/functional_inference.py"], check=True)
+    subprocess.run(["python", os.path.join(yucca.__path__[0], "documentation/templates/functional_inference.py")], check=True)
