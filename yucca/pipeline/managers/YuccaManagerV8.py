78204db6d4ae90bea3acb9d5784780841112ae18
Sllambias <llambias@live.com>
2024-09-09 17:24:12 +0200

    remove stray testing file

--- yucca/pipeline/managers/YuccaManagerV8.py
+++
@@ -1,9 +0,0 @@
-from yucca.pipeline.managers.YuccaManager import YuccaManager
-from yucca.modules.data.augmentation.augmentation_presets import generic
-
-
-class YuccaManagerV8(YuccaManager):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.deep_supervision = True
-        self.augmentation_params = generic
