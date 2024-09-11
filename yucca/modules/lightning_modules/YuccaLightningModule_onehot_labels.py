4d29037f95509c2844f283a9e9c43b2bc9ba5f3c
Asbjørn Munk <9844416+asbjrnmunk@users.noreply.github.com>
2024-09-11 17:20:39 +0200

    Fix multiple loss fn

--- yucca/modules/lightning_modules/YuccaLightningModule_onehot_labels.py
+++ yucca/modules/lightning_modules/YuccaLightningModule_onehot_labels.py
@@ -4,6 +4,7 @@ from torchmetrics import MetricCollection
 from torchmetrics.segmentation import GeneralizedDiceScore
 from torchmetrics.classification import MulticlassF1Score
 from yucca.modules.lightning_modules.YuccaLightningModule import YuccaLightningModule
+from yucca.modules.optimization.loss_functions.combined_losses import SigmoidDiceBCE
 
 
 class YuccaLightningModule_onehot_labels(YuccaLightningModule):
@@ -15,11 +16,13 @@ class YuccaLightningModule_onehot_labels(YuccaLightningModule):
     ):
         super().__init__(
             config=config,
-            loss_fn="SigmoidDiceBCE",
             *args,
             **kwargs,
         )
         # self.regions_labeled = config["regions_labeled"] currently not used, but can be used during inference to go from regions -> labels
+        self.loss_fn = SigmoidDiceBCE
+
+        assert self.num_classes > 1, self.num_classes
 
         self.train_metrics = MetricCollection(
             {
@@ -74,6 +77,12 @@ class YuccaLightningModule_onehot_labels(YuccaLightningModule):
     def validation_step(self, batch, batch_idx):
         inputs, target, file_path = batch["image"], batch["label"], batch["file_path"]
         output = self(inputs)
+
+        print("inputs", inputs.shape)
+        print("outputs", output.shape)
+        print("target", target.shape)
+        print("num classes", self.num_classes)
+
         loss = self.loss_fn_val(output, target)
 
         output = (torch.sigmoid(output) > 0.5).long()
