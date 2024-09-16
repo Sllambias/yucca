f05c56a5cb3a49018b066edc573808fde6481be9
zcr545 <snl@di.ku.dk>
2024-09-16 22:40:55 +0200

    fix bad push

--- yucca/pipeline/configuration/configure_plans.py
+++ yucca/pipeline/configuration/configure_plans.py
@@ -72,7 +72,7 @@ def get_plan_config(
         num_classes = len(regions)
     else:
         labels = plans["dataset_properties"]["labels"]
-        num_classes = max(1, or len(plans["dataset_properties"]["classes"]))
+        num_classes = max(1, len(plans["dataset_properties"]["classes"]))
     image_extension = plans.get("image_extension") or plans["dataset_properties"].get("image_extension") or "nii.gz"
     allow_missing_modalities = plans.get("allow_missing_modalities") or False
 
