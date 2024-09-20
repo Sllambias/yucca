7e1731d98155ff80b9ee2b890d602231ed5a6328
Julia Machnio <julia.machnio1999@gmail.com>
2024-09-20 13:36:46 +0200

    fixed KeyError (probably)

--- yucca/modules/data/augmentation/transforms/Skeleton.py
+++ yucca/modules/data/augmentation/transforms/Skeleton.py
@@ -13,20 +13,15 @@ class Skeleton(YuccaTransform):
         # No parameters to retrieve
         pass
 
-    def __skeleton__(self, array):
-        array_new = array.copy()
-        for b in range(array_new.shape[0]):
-            for c in range(array_new.shape[1]):
-                array_new[b, c] = skeleton(array[b, c])
-        return array_new[0]
+    def __skeleton__(self, label):
+        label_skeleton = label.copy()
+        for b in range(label.shape[0]):
+            for c in range(label.shape[1]):
+                label_skeleton[b, c] = skeleton(label[b, c])
+        return label_skeleton[0]
 
     def __call__(self, packed_data_dict=None, **unpacked_data_dict):
         data_dict = packed_data_dict if packed_data_dict else unpacked_data_dict
-        assert (
-            len(data_dict[self.label_key].shape) == 5 or len(data_dict[self.label_key].shape) == 4
-        ), f"Incorrect data size or shape.\
-            \nShould be (b, c, x, y, z) or (b, c, x, y) and is: {data_dict[self.label_key].shape}"
-
         if self.__skeleton__:
             data_dict["skel"] = self.__skeleton__(data_dict[self.label_key])
         return data_dict
