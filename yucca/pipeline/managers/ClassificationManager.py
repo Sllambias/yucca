ba9499afae41d54822c2d70198ad7fe7847b1cfb
LlambiasMBP <llambias@live.com>
2024-12-23 11:22:27 +0100

    add 30 and 31

--- yucca/pipeline/managers/ClassificationManager.py
+++ yucca/pipeline/managers/ClassificationManager.py
@@ -455,3 +455,99 @@ class ClassificationManagerV15_7(ClassificationManagerV15):
         super().__init__(*args, **kwargs)
         self.optim_kwargs["weight_decay"] = 9e-3
         self.optim_kwargs["lr"] = 2e-6
+
+
+class ClassificationManagerV30(YuccaManager):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model_name = "resnet18"
+        self.optim_kwargs["lr"] = 5e-6
+        self.optim_kwargs["weight_decay"] = 5e-3
+        self.augmentation_params = self.set_aug_params()
+        self.loss = CE
+        self.lightning_module = ClassificationLightningModule
+        self.model_dimensions = "3D"
+        self.patch_based_training = False
+        self.deep_supervision = False
+        self.train_dataset_class = ClassificationTrainDataset
+        self.test_dataset_class = ClassificationTestDataset
+
+    def set_aug_params(self):
+        return {
+            "random_crop": True,
+            "mask_image_for_reconstruction": False,
+            "clip_to_input_range": True,  # ensures no augmentations go beyond the input range of the image/patch
+            "normalize": False,
+            # label/segmentation transforms
+            "skip_label": True,
+            "label_dtype": int,
+            "copy_image_to_label": False,
+            # default augmentation probabilities
+            "additive_noise_p_per_sample": 0.5,
+            "biasfield_p_per_sample": 0.5,
+            "blurring_p_per_sample": 0.4,
+            "blurring_p_per_channel": 0.5,
+            "elastic_deform_p_per_sample": 0.1,
+            "gamma_p_per_sample": 0.4,
+            "gamma_p_invert_image": 0.05,
+            "gibbs_ringing_p_per_sample": 0.3,
+            "mirror_p_per_sample": 0.0,
+            "mirror_p_per_axis": 0.33,
+            "motion_ghosting_p_per_sample": 0.3,
+            "multiplicative_noise_p_per_sample": 0.5,
+            "rotation_p_per_sample": 0.33,
+            "rotation_p_per_axis": 0.66,
+            "scale_p_per_sample": 0.33,
+            "simulate_lowres_p_per_sample": 0.3,
+            "simulate_lowres_p_per_channel": 0.5,
+            "simulate_lowres_p_per_axis": 0.66,
+            # default augmentation values
+        }
+
+
+class ClassificationManagerV31(YuccaManager):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model_name = "resnet18"
+        self.optim_kwargs["lr"] = 5e-5
+        self.optim_kwargs["weight_decay"] = 5e-3
+        self.augmentation_params = self.set_aug_params()
+        self.loss = CE
+        self.lightning_module = ClassificationLightningModule
+        self.model_dimensions = "3D"
+        self.patch_based_training = False
+        self.deep_supervision = False
+        self.train_dataset_class = ClassificationTrainDataset
+        self.test_dataset_class = ClassificationTestDataset
+
+    def set_aug_params(self):
+        return {
+            "random_crop": True,
+            "mask_image_for_reconstruction": False,
+            "clip_to_input_range": True,  # ensures no augmentations go beyond the input range of the image/patch
+            "normalize": False,
+            # label/segmentation transforms
+            "skip_label": True,
+            "label_dtype": int,
+            "copy_image_to_label": False,
+            # default augmentation probabilities
+            "additive_noise_p_per_sample": 0.2,
+            "biasfield_p_per_sample": 0.2,
+            "blurring_p_per_sample": 0.2,
+            "blurring_p_per_channel": 0.5,
+            "elastic_deform_p_per_sample": 0.01,
+            "gamma_p_per_sample": 0.2,
+            "gamma_p_invert_image": 0.05,
+            "gibbs_ringing_p_per_sample": 0.2,
+            "mirror_p_per_sample": 0.0,
+            "mirror_p_per_axis": 0.33,
+            "motion_ghosting_p_per_sample": 0.1,
+            "multiplicative_noise_p_per_sample": 0.2,
+            "rotation_p_per_sample": 0.15,
+            "rotation_p_per_axis": 0.66,
+            "scale_p_per_sample": 0.33,
+            "simulate_lowres_p_per_sample": 0.2,
+            "simulate_lowres_p_per_channel": 0.5,
+            "simulate_lowres_p_per_axis": 0.66,
+            # default augmentation values
+        }
