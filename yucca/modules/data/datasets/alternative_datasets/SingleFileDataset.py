0e82b8f32b802e64b0486e94d1430073346e715a
LlambiasMBP <llambias@live.com>
2025-04-11 10:58:59 +0200

    Capital letters, man..

--- yucca/modules/data/datasets/alternative_datasets/SingleFileDataset.py
+++ yucca/modules/data/datasets/alternative_datasets/SingleFileDataset.py
@@ -2,7 +2,7 @@ import torch
 
 
 class SingleFileTestDataset(torch.utils.data.Dataset):
-    def __init__(self, raw_data_dir: str, pred_save_dir: str, suffix=".nii.gz", **kwargs):  # noqa: u100
+    def __init__(self, raw_data_dir: str, pred_save_dir: str, suffix=".nii.gz", **kwargs):  # noqa: U100
         self.data_path = raw_data_dir
         self.pred_save_dir = pred_save_dir
         self.suffix = suffix
@@ -10,7 +10,7 @@ class SingleFileTestDataset(torch.utils.data.Dataset):
     def __len__(self):
         return 1
 
-    def __getitem__(self, idx):  # noqa: u100
+    def __getitem__(self, idx):  # noqa: U100
         # Here we generate the paths to the cases along with their ID which they will be saved as.
         # we pass "case" as a list of strings and case_id as a string to the dataloader which
         # will convert them to a list of tuples of strings and a tuple of a string.
