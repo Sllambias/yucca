c416e8978ddf363724c0d1dbb6ea6f599035aab4
zcr545 <snl@di.ku.dk>
2025-05-09 09:45:42 +0200

    Add valid Nib type to type checker

--- yucca/functional/utils/type_conversions.py
+++ yucca/functional/utils/type_conversions.py
@@ -14,7 +14,7 @@ def np_to_nifti_with_empty_header(array):
 def nifti_or_np_to_np(array: Union[np.ndarray, nib.Nifti1Image]) -> np.ndarray:
     if isinstance(array, np.ndarray):
         return array
-    if isinstance(array, nib.Nifti1Image):
+    if isinstance(array, (nib.Nifti1Image, nib.minc1.Minc1Image)):
         return array.get_fdata().astype(np.float32)
     else:
         raise TypeError(f"File data type invalid. Found: {type(array)} and expected nib.Nifti1Image or np.ndarray")
