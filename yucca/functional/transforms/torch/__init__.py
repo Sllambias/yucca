3ba6e06a7586e316ebdb07073b89e6a7cd2a2d08
Benjamin Starostka Jakobsen <benjamin.starostka@gmail.com>
2025-05-16 15:20:13 +0200

    module loads

--- yucca/functional/transforms/torch/__init__.py
+++ yucca/functional/transforms/torch/__init__.py
@@ -1,3 +1,5 @@
 from .croppad import torch_croppad
 from .bias_field import torch_bias_field
 from .blur import torch_blur
+from .gamma import torch_gamma
+from .motion_ghosting import torch_motion_ghosting
