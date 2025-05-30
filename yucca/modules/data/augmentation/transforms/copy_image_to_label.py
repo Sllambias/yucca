9807abfed48ddbb1f187266b63cacb2f9ff39f88
zcr545 <snl@di.ku.dk>
2025-05-30 09:28:02 +0200

    Add: copy

--- yucca/modules/data/augmentation/transforms/copy_image_to_label.py
+++ yucca/modules/data/augmentation/transforms/copy_image_to_label.py
@@ -25,3 +25,25 @@ class CopyImageToLabel(YuccaTransform):
         if self.copy:
             data_dict = self.__copy__(data_dict)
         return data_dict
+
+
+class Torch_CopyImageToLabel(YuccaTransform):
+    def __init__(self, copy=False, data_key="image", label_key="label"):
+        self.copy = copy
+        self.data_key = data_key
+        self.label_key = label_key
+
+    @staticmethod
+    def get_params():
+        # No parameters to retrieve
+        pass
+
+    def __copy__(self, data_dict):
+        data_dict[self.label_key] = data_dict[self.data_key].detach().clone()
+        return data_dict
+
+    def __call__(self, packed_data_dict=None, **unpacked_data_dict):
+        data_dict = packed_data_dict if packed_data_dict else unpacked_data_dict
+        if self.copy:
+            data_dict = self.__copy__(data_dict)
+        return data_dict
