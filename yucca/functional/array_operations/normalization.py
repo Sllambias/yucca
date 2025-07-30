03ba8c00ce5f9380aabc9b54a06781cb745721d0
zcr545 <snl@di.ku.dk>
2025-07-30 16:01:12 +0200

    Bump minor patch

--- yucca/functional/array_operations/normalization.py
+++ yucca/functional/array_operations/normalization.py
@@ -15,7 +15,7 @@ def normalizer(array: np.ndarray, scheme: str, intensities: Optional[dict] = Non
     Clip = for contrast clipping. This will clip values to the 0.01 and 99.99th percentiles
         and then perform 0-1 normalization.
     """
-    accepted_schemes = ["clipping", "ct", "minmax", "range", "no_norm", "standardize", "volume_wise_znorm", "255to1"]
+    accepted_schemes = ["clipping", "ct", "minmax", "range", "no_norm", "standardize", "volume_wise_znorm", "255to1", "ct_brain_norm"]
 
     assert scheme in accepted_schemes, "invalid normalization scheme inserted" f"attempted scheme: {scheme}"
     assert array is not None
